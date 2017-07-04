b4ed2584ee54c626198fe0b0c4a3b9c08fa9cace
kexxii <kexxii@gmail.hu>
2017-07-04 22:18:49 +0200

    Index.php of Website.

---
+++ index.php
@@ -0,0 +1,128 @@
+
+<!--[**************]-->
+<!--[ MINECRAFT RW ]-->
+<!--[**************]-->
+<html>
+<head>
+     <meta charset="utf-8">
+     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Amatica+SC|Geo|Press+Start+2P" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+<meta charset="utf-8">
+<title>MINECRAFT RW</title>
+</head>
+<body>
+
+
+<div class="container lightGreen pullDown">
+  <a>KEZDŐLAP</a>
+  <a>CSAPAT</a>
+  <a>SZERVERRŐL</a>
+</div>
+
+</body>
+<style>
+
+body {
+	font-family: 'Roboto', sans-serif;
+}
+
+
+@media (min-width: 768px){
+                .col-sm-12{
+                    float:left;
+                }
+            }
+            .col-xs-12{
+                    float:left;
+                }
+
+.content3 {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-size: 12px;
+    border-top: 3px solid #333;
+    padding: 0px 10px 10px 10px;
+    background-color: rgba(255, 255, 255, 0.9);
+    box-shadow: 0px 5px 5px #333;
+}
+
+.statcontent {
+    border-top: 3px solid black;
+    margin-bottom: 20px;
+	margin: 25px;
+	background-color: white;
+	background-color: rgba(255, 255, 255, 0.9);
+    box-shadow: 0px 5px 5px #333;
+}
+
+body {
+  /* font-family: 'Questrial', sans-serif; */
+  background-image: url(images/rw3.jpg)
+}
+
+a {
+  color: black;
+}
+
+::selection {
+  background-color: black;
+  color: white;
+}
+
+p {
+  font-family: 'Open Sans Condensed', sans-serif;
+  text-align: center;
+  font-size: 35px;
+}
+
+div.container
+{
+    font-family: 'Roboto', sans-serif;
+    margin: 0 auto;
+    padding: 10em 3em;
+    text-align: center;
+}
+
+div.container a
+{
+    color: #FFF;
+    text-decoration: none;
+    font: 20px 'Roboto';
+    margin: 0px 10px;
+    padding: 10px 10px;
+    position: relative;
+    z-index: 0;
+    cursor: pointer;
+}
+
+/* Pull down  */
+div.pullDown a:before
+{
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    left: 0px;
+    top: 0px;
+    content: '';
+    background: white;
+    opacity: 0.3;
+    transition: all 0.3s;
+}
+
+div.pullDown a:hover:before
+{
+    height: 100%;
+}
+
+</style>
+</html>
